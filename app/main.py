03a6813cd0a6095d74fd15c2b60507637fc4cc66
munited05 <m_ghannam95@hotmail.com>
2017-03-03 12:05:02 -0800

    change direction to down

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def move():
     directions = ['up', 'down', 'left', 'right']
 
     return {
-        'move': random.choice(directions),
+        'move': 'down',
         'taunt': 'battlesnake-python!'
     }
 
